ed7b86d740533422cc43504aae02fd3e8d0d58c1
Ryan Santos <Ryluna19gmail.com>
2026-06-11 11:29:42 -0300

    upadate personal profile page

--- Script.js
+++ Script.js
@@ -1,16 +1,14 @@
 function toggleMode() {
-  const html = document.documentElement
-  html.classList.toggle("light")
-  const img = document.querySelector('#profile img')
-  
+  const html = document.documentElement;
+  const img = document.querySelector("#profile img");
 
-  if(html.classList.contains('light')){
-    img.setAttribute('src','assets/avatar.png')
+  html.classList.toggle("light");
 
-  }else{
-    img.setAttribute("src", "assets/avatar-light.png")
-    img.setAttribute("alt", '"Ryan Santos , Perfil , Sorrindo"')
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "assets/avatar.png");
+    img.setAttribute("alt", "Foto de Ryan Santos");
+  } else {
+    img.setAttribute("src", "assets/avatar-light.png");
+    img.setAttribute("alt", "Foto de Ryan Santos");
   }
-
-
 }
